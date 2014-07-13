86733ed51c7dfc3bb6d84e3d1c27accc4dec5505
Ludovico Russo <ludus.russo@gmail.com>
2014-07-13 21:25:04 +0200

    edit hand.ino

--- arduino/hand.ino
+++ arduino/hand.ino
@@ -1,18 +1,59 @@
-
-
 #include <Servo.h> 
- 
+
 Servo indice;  
 Servo medio;
 Servo anulare;
 Servo mignolo;
 Servo pollice;
 Servo polso;
-    
+
 int Sbyte, indiceByte, medioByte, anulareByte, mignoloByte, polliceByte, polsoByte;                
- 
+
 int pos = 0, migntemp=0;    
- 
+
+
+
+int all_control() {
+  indiceByte =  Serial.read();
+  medioByte =   Serial.read();
+  anulareByte = Serial.read();
+  mignoloByte = Serial.read();
+  polliceByte = Serial.read();
+  polsoByte =   Serial.read();
+
+
+  //servo control
+  if(indiceByte>=0 && indiceByte<=180){
+    indice.write(indiceByte);    
+  }
+
+  if(medioByte>=0 && medioByte<=180){
+    medio.write(medioByte);    
+  }
+
+  if(anulareByte>=0 && anulareByte<=180){
+    anulare.write(anulareByte);    
+  }
+
+  if(mignoloByte>=0 && mignoloByte<=180){
+    migntemp=abs(mignoloByte-180);
+    if(migntemp>160)
+      migntemp=160;
+    if(migntemp<50)
+      migntemp=50;
+    mignolo.write(migntemp);    
+  }
+
+  if(polliceByte>=0 && polliceByte<=180){
+    pollice.write(polliceByte);    
+  }
+
+  if(polsoByte>=0 && polsoByte<=180){
+    polso.write(polsoByte);    
+  }
+}
+
+
 void setup() 
 { 
   indice.attach(13);
@@ -21,55 +62,18 @@ void setup()
   mignolo.attach(3);
   pollice.attach(2);
   polso.attach(4);
-  
+
   Serial.begin(9600);
 } 
- 
- 
+
+
 void loop() 
 { 
-      if (Serial.available() >= 7) {
-     Sbyte = Serial.read();//byte inizio
-    
-            if (Sbyte == 255){
-               indiceByte = Serial.read(); //pacchetto dati
-               medioByte = Serial.read();
-               anulareByte = Serial.read();
-               mignoloByte = Serial.read();
-               polliceByte =Serial.read();
-               polsoByte = Serial.read();
-       
-       
-               //pilotiamo i servo
-               if(indiceByte>=0 && indiceByte<=180){
-                     indice.write(indiceByte);    
-               }
-               if(medioByte>=0 && medioByte<=180){
-                     medio.write(medioByte);    
-               }
-               if(anulareByte>=0 && anulareByte<=180){
-                     anulare.write(anulareByte);    
-               }
-               if(mignoloByte>=0 && mignoloByte<=180){
-                 migntemp=abs(mignoloByte-180);
-                 if(migntemp>160)
-                 migntemp=160;
-                 if(migntemp<50)
-                 migntemp=50;
-                     mignolo.write(migntemp);    
-               }
-               if(polliceByte>=0 && polliceByte<=180){
-                     pollice.write(polliceByte);    
-               }
-                if(polsoByte>=0 && polsoByte<=180){
-                     polso.write(polsoByte);    
-               }
+  if (Serial.available() >= 7) {
+    Sbyte = Serial.read();//byte inizio
 
-
-            }
-  
-      }
-      
-      
-      
+    if (Sbyte == 255){
+      all_control();
+    }
+  }
 } 
