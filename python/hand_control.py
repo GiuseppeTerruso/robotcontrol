b27274302d1988a9b39a5f3ae6d54e31a0595fa5
Ludovico Russo <ludus.russo@gmail.com>
2014-07-24 23:43:25 +0200

    corrected license

--- python/hand_control.py
+++ python/hand_control.py
@@ -1,19 +1,29 @@
-# Copyright (c) 2014 CINI Consorzio Interuniversitario Nazionale di Informatica
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#! /usr/bin/env python
+
+# Copyright (C) 2014 Politecnico di Torino
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# This software is developed within the PARLOMA project, which aims
+# at developing a communication system for deablinf people (www.parloma.com)
+# The PARLOMA project is developed with the Turin node of AsTech laboraroies
+# network of Italian CINI (Consorzio Interuniversitario Nazionale di Informatica)
+
 # Contributors:
-#
-#       Ludovico Russo
+#     Ludovico O. Russo (ludovico.russo@polito.it)
+
 
 import serial
 import time
@@ -27,10 +37,6 @@ def constant(prop):
 
 
 class Hand:
-    sign_set = {'s':[   0, 180, 180, 180, 180,   0], 'v':[ 180,   0,   0, 180, 180,   0], 'w':[ 180,   0,   0,   0, 180,   0], 'corna':[    0,  170, 170,   0, 170], 'fuck':[   170,    0,  170,    170,    0], 'rock':[ 170, 170, 170, 0, 0], 'pugno':[    160,    160,    160,    160,    160,    160], 'rest':[   0,   0,   0,   0,   0,   0], 'f':[ 90, 100, 0,   0, 0,   50]}
-
-
-
 
     def __init__(self, hand_port):
         self.serial_comm = serial.Serial(port=hand_port, baudrate=9600)
@@ -58,29 +64,3 @@ class Hand:
     def set_all_position(self, positions):
         cmds = [255] + positions
         self.send_command(cmds)
-
-    def perform_sign(self, sign):
-        if (sign in self.sign_set):
-            self.set_all_position(self.sign_set[sign])
-
-    def demo_bulga(self):
-        self.perform_sign('s')
-        time.sleep(2)
-        self.perform_sign('v')
-        time.sleep(2)
-        self.perform_sign('w')
-        time.sleep(2)
-        self.perform_sign('f')
-        time.sleep(2)
-        self.perform_sign('pugno')
-        time.sleep(2)
-        self.perform_sign('rest')
-        time.sleep(2)
-        self.perform_sign('corna')
-        time.sleep(2)
-        self.perform_sign('rock')
-        time.sleep(2)
-        self.perform_sign('fuck')
-        time.sleep(2)
-        self.perform_sign('rest')
-        time.sleep(2)
