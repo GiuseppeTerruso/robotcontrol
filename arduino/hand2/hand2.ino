7a5eed771f3a0d0406bde7245421220e9a8faed1
Ludovico Russo <ludus.russo@gmail.com>
2014-09-28 21:50:21 +0200

    hand2 working

--- arduino/hand2/hand2.ino
+++ arduino/hand2/hand2.ino
@@ -26,30 +26,90 @@
 
 
 #include <Servo.h>
+#include <EEPROM.h>
+
+#define CONFIG_VERSION  "ls1"
+#define CONFIG_START    32
 
 
+#define thumb_flex      0
+#define index_flex      1
+#define middle_flex     2
+#define ring_flex       3
+#define pinky_flex      4
+#define thumb_abd       5
+#define index_abd       6
+#define middle_abd      7
+#define wrist_1         8
+#define MOTOR_NUM       9
 
-enum motor_t {thumb_flex, index_flex, middle_flex, ring_flex, pinky_flex,
-              thumb_abd, index_abd, middle_abd, wrist_1,  MOTOR_NUM
-              };
+
+struct StoreStruct {
+  char version[4];
+  char reset[MOTOR_NUM];
+};
+
+struct StoreStruct storeReset = {
+  CONFIG_VERSION,
+  {180,180,180,180,180,180,90,90,150}
+};
+
+void loadConfig() {
+  // To make sure there are settings, and they are YOURS!
+  // If nothing is found it will use the default settings.
+  if (EEPROM.read(CONFIG_START + 0) == CONFIG_VERSION[0] &&
+      EEPROM.read(CONFIG_START + 1) == CONFIG_VERSION[1] &&
+      EEPROM.read(CONFIG_START + 2) == CONFIG_VERSION[2])
+    for (unsigned int t=0; t<sizeof(storeReset); t++)
+      *((char*)&storeReset + t) = EEPROM.read(CONFIG_START + t);
+}
+
+void saveConfig() {
+  for (unsigned int t=0; t<sizeof(storeReset); t++)
+    EEPROM.write(CONFIG_START + t, *((char*)&storeReset + t));
+}
+
+
+const int   SET_ALL              = 241;
+const int   SET_ONE              = 242;
+const int   GET_ALL_POS          = 243;
+const int   GET_FINGER_POS       = 244;
+const int   RESET_ALL            = 245;
+const int   TEST_ALL             = 246;
+const int   SAVE_RESET_ALL        = 247;
+
 
 class Motor{
   public:
-    Motor(int pin, int pos = 0, int p_min = 0, int p_max = 180, bool direct = true);
+    Motor();
+    void init(int pin, int pos = 0, int p_min = 0, int p_max = 180, bool direct = true);
     void reset();
-    void move(int pos);
+    void setPosition(int pos);
+    int get_mpos();
+    void test();
+    void set_reset(int res);
+
   private:
     Servo motor;
-    int position;
+    int mpos;
     int p_max;
     int p_min;
     int pos_reset;
     bool direct;
 };
 
-Motor::Motor(int pin, int pos, int p_min, int p_max, bool direct):
-    position(-1), p_max(p_max), p_min(p_min), direct(direct)
+Motor::Motor(): mpos(-1) {}
+
+void Motor::set_reset(int res) {
+  pos_reset = res;
+}
+
+
+void Motor::init(int pin, int pos, int p_min, int p_max, bool direct)
 {
+  this->p_max = p_max;
+  this->p_min = p_min;
+  this->direct = direct;
   if (pos >= 0 && pos <= 180) {
     pos_reset = pos;
   } else {
@@ -59,57 +119,159 @@ Motor::Motor(int pin, int pos, int p_min, int p_max, bool direct):
   this->reset();
 }
 
+
+
 void Motor::reset() {
-  this->move(pos_reset);
+  this->setPosition(pos_reset);
 }
 
-void Motor::move(int pos) {
-   if (pos >= 0 && pos <= 180) {
-   if (this->direct) pos = 180-pos;
-    position = map(pos, 0, 180, p_min, p_max);
-    motor.write(position);
+void Motor::setPosition(int pos) {
+  if (pos >= 0 && pos <= 180) {
+    if (!this->direct) pos = 180-pos;
+    this->mpos = map(pos, 0, 180, p_min, p_max);
+    motor.write(this->mpos);
   }
 }
 
-Motor *motors[MOTOR_NUM];
+int Motor::get_mpos(){
+  return map(mpos, p_min, p_max, 0, 180);
+}
+
+void Motor::test() {
+  int thispose = this->mpos;
+  for (int i = thispose; i <= 180; i++) {
+    this->setPosition(i);
+    delay(5);
+  }
+  for (int i = 180; i >= 0; --i) {
+      this->setPosition(i);
+      delay(5);
+  }
+  for (int i = 0; i <= thispose; ++i) {
+      this->setPosition(i);
+      delay(5);
+  }
+}
+
+Motor motors[MOTOR_NUM];
 
 
 void allocate_motors() {
-    motors[thumb_flex]  =   new Motor(  10,   0,  0,  180,  true);
-    motors[index_flex]  =   new Motor(  12,   0,  0,  180,  true);
-    motors[middle_flex] =   new Motor(  5,    0,  0,  180,  true);
-    motors[ring_flex]   =   new Motor(  4,    0,  0,  180,  true);
-    motors[pinky_flex]  =   new Motor(  3,    0,  0,  180,  true);
-    motors[thumb_abd]   =   new Motor(  6,    0,  0,  180,  true);
-    motors[index_abd]   =   new Motor(  11,   0,  0,  180,  true);
-    motors[middle_abd]  =   new Motor(  2,    0,  0,  180,  true);
-    motors[wrist_1]     =   new Motor(  8,    0,  0,  180,  true);
+  motors[thumb_flex].init(  8,   storeReset.reset[thumb_flex],  10,  170,  true);
+  motors[index_flex].init(  4,   storeReset.reset[index_flex],  10,  170,  true);
+  motors[middle_flex].init(  5,   storeReset.reset[middle_flex],  10,  170,  true);
+  motors[ring_flex].init(  3,   storeReset.reset[ring_flex],  10,  170,  false);
+  motors[pinky_flex].init(  6,   storeReset.reset[pinky_flex],  10,  180,  true);
+  motors[thumb_abd].init(  11,   storeReset.reset[thumb_abd],  10,  170,  true);
+  motors[index_abd].init(  2,   storeReset.reset[index_abd],  10,  170,  true);
+  motors[middle_abd].init(  12,   storeReset.reset[middle_abd],  10,  170,  true);
+  motors[wrist_1].init(  10,   storeReset.reset[wrist_1],  10,  170,  true);
 }
 
-int Sbyte, indiceByte, medioByte, anulareByte, mignoloByte, polliceByte, polsoByte, adindiceByte, admedioByte, adpolliceByte;
 
-char command; // 201 = control all fingers
-              // 202 = control specific finger
+void setPosition_all() {
+
+ while(Serial.available() < MOTOR_NUM);
+ int val[MOTOR_NUM];
+ for (int i = 0; i < MOTOR_NUM; i++) {
+    val[i] = Serial.read();
+  }
+
+  for (int i = 0; i < MOTOR_NUM; i++) {
+    motors[i].setPosition(val[i]);
+  }
+}
+
+void reset_all() {
+  for (int i = 0; i < MOTOR_NUM; i++) {
+    motors[i].reset();
+  }
+}
 
 
-int pos = 0, migntemp=0;
+void setPosition_one() {
+  while (Serial.available() < 2);
+  int tosetPosition = Serial.read();
+  if (tosetPosition < 0 ) return;
+  if (tosetPosition < MOTOR_NUM) {
+    int pos = Serial.read();
+    motors[tosetPosition].setPosition(pos);
+  }
+}
+
+void get_all_mpos() {
+  for (int i = 0; i < MOTOR_NUM; i++) {
+    Serial.write((char) motors[i].get_mpos());
+  }
+}
+
+void get_finger_mpos() {
+  if (Serial.available() < 1) {
+    return;
+  }
+  int toget = Serial.read();
+  if (toget < MOTOR_NUM) {
+    Serial.write((char) motors[toget].get_mpos());
+  }
+}
+
+void test_motor(int mot) {
+  if (mot < MOTOR_NUM) {
+     for (int i = 0; i < 180; i++) {
+        motors[mot].setPosition((i+120)%180);
+        delay(10);
+    }
+  }
+}
+
+void test_all(){
+  for (int i = 0; i < MOTOR_NUM; i++) {
+    motors[i].test();
+  }
+}
+
+void save_reset_all(){
+  for (int i = 0; i < MOTOR_NUM; i++) {
+    storeReset.reset[i] = motors[i].get_mpos();
+    motors[i].set_reset(storeReset.reset[i]);
+  }
+  saveConfig();
+}
+
 
 void setup()
 {
+  loadConfig();
   allocate_motors();
   Serial.begin(9600);
 }
 
-
 void loop()
 {
-  if (Serial.available() >= 10) {
-    Sbyte = Serial.read();//byte inizio
-
-    if (Sbyte == 245) {
-      for (int i = 0; i < 9; i++) {
-        motors[i]->move(Serial.read());
-      }
+  if (Serial.available()) {
+    int Sbyte = Serial.read(); //byte inizio
+    switch (Sbyte) {
+      case SET_ALL:
+        setPosition_all();
+        break;
+      case TEST_ALL:
+          test_all();
+        break;
+      case SET_ONE:
+        setPosition_one();
+        break;
+      case GET_FINGER_POS:
+        get_finger_mpos();
+        break;
+      case GET_ALL_POS:
+        get_all_mpos();
+        break;
+      case RESET_ALL:
+        reset_all();
+        break;
+      case SAVE_RESET_ALL:
+        save_reset_all();
+        break;
     }
   }
 }
